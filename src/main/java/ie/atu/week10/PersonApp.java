7b31d7f102b7ee6e078a33aed7c6d56822dfc3f8
g00401655 <g00401655@atu.ie>
2022-11-29 13:41:07 +0000

    Added Person class and Customer

---
+++ src/main/java/ie/atu/week10/PersonApp.java
@@ -0,0 +1,8 @@
+package ie.atu.week10;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person bob = new Person("bob","Galway","0850229812");
+        System.out.println(bob.toString());
+    }
+}
